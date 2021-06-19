07c253d33453aeef032b297212a68f49562210ba
Jumpei Mori <nikutto.kom@gmail.com>
2021-06-19 19:25:05 +0900

    remove archived method

--- src/ServerUtil.tsx
+++ src/ServerUtil.tsx
@@ -1,5 +1,3 @@
-import Landmark from './Landmark'
-import Transportation from './Transportation'
 import Site from './Site'
 
 module ServerUtil {
@@ -8,26 +6,6 @@ module ServerUtil {
         return fetch('http://localhost:8080/' + path)
     }
     
-    export async function getLandmarksOf(day: number) {
-        const response = await fetchServer('landmark?day=' + day)
-        const json: any[] = await response.json()
-        const landmarks: Landmark[] = json.map(e => {
-            let ret: Landmark = { id: e['id'], name: e['name'], detail: e['detail'] }
-            return ret
-        })
-        return landmarks
-    }
-
-    export async function getTransportationsOf(day: number) {
-        const response = await fetchServer('/transportation?day=' + day)
-        const json: any[] = await response.json()
-        const transportations: Transportation[] = json.map(e => {
-            let ret: Transportation = { name: e['name'] }
-            return ret
-        })
-        return transportations
-    }
-
     export async function getSitesOf(day: number) {
         const response = await fetchServer('/site?day=' + day)
         const sites: Site[] = await response.json()
